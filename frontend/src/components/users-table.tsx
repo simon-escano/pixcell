7eb1aa3d238a4489fd128712c30d7d3495208d55
moltmalt <moltsolon@gmail.com>
2025-06-16 17:15:19 +0800

    Fixed order of users table

--- frontend/src/components/users-table.tsx
+++ frontend/src/components/users-table.tsx
@@ -32,13 +32,13 @@ import { PhoneInput } from "./ui/phone-input";
 
 type CombinedUser = {
   id: User["id"];
-  email: User["email"];
-  phone: User["phone"];
   firstName: Profile["firstName"];
   lastName: Profile["lastName"];
-  imageUrl: Profile["imageUrl"];
-  roleId: Role["id"];
+  email: User["email"];
   roleName: Role["name"];
+  phone: User["phone"];
+  imageId: Profile["imageId"];
+  roleId: Role["id"];
 };
 
 export const UsersTable = ({ users }: { users: CombinedUser[] }) => {
@@ -134,8 +134,8 @@ export const UsersTable = ({ users }: { users: CombinedUser[] }) => {
     <div>
       <DataTable
         data={users}
-        excludeColumns={["roleId"]}
-        columnConfigs={[{ key: "imageUrl", maxWidth: 200 }]}
+        excludeColumns={["roleId", "phone", "imageId", "imageUrl", "id"]}
+        columnConfigs={[{ key: "imageId", maxWidth: 200 }]}
         actionItems={actionItems}
         onRowClick={(user: CombinedUser) => {
           router.push(`/users/${user.id}`);
@@ -157,7 +157,7 @@ export const UsersTable = ({ users }: { users: CombinedUser[] }) => {
                 <div className="group relative cursor-pointer">
                   <Avatar className="size-24">
                     <AvatarImage
-                      src={preview || selectedUser?.imageUrl || ""}
+                      src={preview || selectedUser?.imageId || ""}
                       className="object-cover"
                     />
                     <AvatarFallback>
