e75593b6481eec68ea648f45e24196545dea7750
simonescano <escanosimonlyster@gmail.com>
2025-07-20 12:35:44 +0800

    refactor: Update Liveblocks interface for improved presence management

--- frontend/liveblocks.config.ts
+++ frontend/liveblocks.config.ts
@@ -1,21 +1,24 @@
-import { Profile } from "@/db/schema";
-import { Color, Layer, Point } from "@/types";
-import { LiveList, LiveMap, LiveObject } from "@liveblocks/client";
+import { LiveMap } from "@liveblocks/core";
+
+export type PresenceStates = "playing" | "seeking" | "paused";
 
 declare global {
   interface Liveblocks {
-    // Each user's Presence, for useMyPresence, useOthers, etc.
+    // Each user's Presence, for room.getPresence, room.subscribe("others"), etc.
     Presence: {
-      profile: Profile
-      selection: string[];
-      cursor: Point | null;
-      pencilDraft: [x: number, y: number, pressure: number][] | null;
-      penColor: Color | null;
+      presence: any; // Used by tldraw
     };
-    // The Storage tree for the room, for useMutation, useStorage, etc.
     Storage: {
-      layers: LiveMap<string, LiveObject<Layer>>;
-      layerIds: LiveList<string>;
+      records: LiveMap<string, any>; // Used by tldraw
+    };
+    // Custom user info set when authenticating with a secret key
+    UserMeta: {
+      id: string; // Accessible through `user.id`
+      info: {
+        name: string;
+        color: string;
+        avatar: string;
+      }; // Accessible through `user.info`
     };
   }
 }
